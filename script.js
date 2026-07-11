5130c4b0e633ed8003bcbe82da6ec7657f3ed6b1
vmaxfaniel-netizen <vmaxfaniel@gmail.com>
2026-07-11 15:59:20 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const ticket = {
+    numero: "ESY-001",
+    date: "Samedi 27",
+    heure: "16h30",
+    lieu: "Aktowers"
+};
 
+document.getElementById("numero").textContent = ticket.numero;
+
+document.querySelector(".ticket p:nth-of-type(1)").textContent = "📅 " + ticket.date;
+document.querySelector(".ticket p:nth-of-type(2)").textContent = "🕟 " + ticket.heure;
+document.querySelector(".ticket p:nth-of-type(3)").textContent = "📍 " + ticket.lieu;
+
+document.getElementById("qr").src = "qrcodes/" + ticket.numero + ".png";
